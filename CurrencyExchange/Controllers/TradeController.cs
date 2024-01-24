2c4cac69768637122c79e1bdacde7584b3a8a052
Ryan Zahra <23ryanzahra@gmail.com>
2024-01-24 23:54:40 +0100

    Added the TradeController

---
+++ CurrencyExchange/Controllers/TradeController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CurrencyExchange.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TradeController : ControllerBase
+    {
+
+    }
+}
