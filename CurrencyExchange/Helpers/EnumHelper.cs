de11b11502f14990e0fc98d0d3893ed4d0c11379
Ryan Zahra <23ryanzahra@gmail.com>
2024-01-24 23:53:40 +0100

    Added an EnumsHelper

---
+++ CurrencyExchange/Helpers/EnumHelper.cs
@@ -0,0 +1,12 @@
+﻿namespace CurrencyExchange.Helpers
+{
+    public class EnumHelper
+    {
+
+        public enum RateProvider
+        {
+            Fixer = 1,
+            ExchangeRatesApi = 2,
+        }
+    }
+}
